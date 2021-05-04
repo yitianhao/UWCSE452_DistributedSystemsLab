d7c51591461002c78a3da4e444466724387e5fb8
Yitian Hao <yitianhao00@gmail.com>
2021-05-05 00:15:46 +0800

    lab3: add phase2

--- labs/lab3-paxos/src/dslabs/paxos/Messages.java
+++ labs/lab3-paxos/src/dslabs/paxos/Messages.java
@@ -1,3 +1,21 @@
 package dslabs.paxos;
 
+import dslabs.atmostonce.AMOCommand;
+import dslabs.framework.Message;
+import dslabs.paxos.PaxosServer.Ballot;
+import lombok.Data;
+
 // Your code here...
+@Data
+class P2A implements Message {
+    // P2A message should contain (ballot, slot number, command)
+    private final Ballot ballot;
+    private final Integer slotNum;
+    private final AMOCommand command;
+}
+
+@Data
+class P2B implements Message {
+    private final Ballot ballot;
+    private final Integer slotNum;
+}
--- labs/lab3-paxos/src/dslabs/paxos/PaxosServer.java
+++ labs/lab3-paxos/src/dslabs/paxos/PaxosServer.java
@@ -1,11 +1,14 @@
 package dslabs.paxos;
 
 import dslabs.atmostonce.AMOApplication;
+import dslabs.atmostonce.AMOCommand;
+import dslabs.atmostonce.AMOResult;
 import dslabs.framework.Address;
 import dslabs.framework.Application;
 import dslabs.framework.Command;
 import dslabs.framework.Node;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -20,7 +23,15 @@ public class PaxosServer extends Node {
 
     // Your code here...
     private final AMOApplication<Application> application;
-    private HashMap<Integer, LogEntry> log;
+    private HashMap<Integer, LogEntry> log;  // logNum -> LogEntry
+    private boolean leader;
+    private int slot_out;
+    private int slot_in;
+    private int seqNum;
+    private Ballot ballot;
+    private HashMap<Integer, AcceptedEntry> accepted;
+    private HashSet<Address> majority;
+    private Address client;
 
     /* -------------------------------------------------------------------------
         Construction and Initialization
@@ -32,12 +43,24 @@ public class PaxosServer extends Node {
         // Your code here...
         this.application = new AMOApplication<>(app);
         this.log = new HashMap<>();
+        slot_in = 0;
+        slot_out = 0;
+        seqNum = 0;
+        ballot = new Ballot(seqNum, address);
+        accepted = new HashMap<>();
+        majority = new HashSet<>();
+//        for (Address a : servers) {
+//            System.out.println(a);
+//
     }
 
 
     @Override
     public void init() {
         // Your code here...
+        if (Objects.equals(address(), servers[servers.length - 1])) {
+            leader = true;
+        }
     }
 
     /* -------------------------------------------------------------------------
@@ -130,9 +153,52 @@ public class PaxosServer extends Node {
        -----------------------------------------------------------------------*/
     private void handlePaxosRequest(PaxosRequest m, Address sender) {
         // Your code here...
+        // As a non-leader, need to drop client request
+        if (leader) {
+            for (Address otherServer : servers) {
+                if (!Objects.equals(address(), otherServer)) {
+                    LogEntry logEntry = new LogEntry(ballot, PaxosLogSlotStatus.ACCEPTED, m.amoCommand());
+                    log.put(slot_in, logEntry);
+                    send(new P2A(ballot, slot_in, m.amoCommand()), otherServer);
+                    // onP2ATimer???
+                }
+            }
+        }
+        client = sender;
     }
 
     // Your code here...
+    // acceptors
+    private void handleP2A(P2A m, Address sender) {
+        if (m.ballot().compareTo(ballot) == 0) { // accept
+            accepted.put(m.slotNum(), new AcceptedEntry(m.ballot(), m.command()));
+            send(new P2B(m.ballot(), m.slotNum()), sender);
+        } else {
+            send(new P2B(null, null), sender);
+        }
+    }
+
+    private void handleP2B(P2B m, Address sender) {
+        if (m.ballot() != null) {
+            majority.add(sender);
+        }
+        if (majority.size() >= servers.length / 2) {
+            if (m.slotNum() != slot_in) {
+                System.out.println("!!!!!!!!!!!!");
+            }
+            Command command = log.get(m.slotNum()).command;
+            log.put(m.slotNum(), new LogEntry(m.ballot(), PaxosLogSlotStatus.CHOSEN, command));
+            AMOResult result = application.execute(command);
+            send(new PaxosReply(result), client);
+        }
+        /**
+         * Need a way to sync logs between leader and all the acceptors to learn about missed decisions.
+         * They can also sync up with heartbeat messages.
+         * Alternatively, when the leader sends an accept message, attach the leader’s log with the request. When acceptor replies,
+         * attach the acceptor’s log with the response. Then update/merge each side’s log.
+         * (Although you should get normal p2a working first.) Lots of different possible protocols and implementations to consider!
+         */
+    }
 
 
     /* -------------------------------------------------------------------------
@@ -148,7 +214,7 @@ public class PaxosServer extends Node {
     /* -------------------------------------------------------------------------
         Inner Classes
        -----------------------------------------------------------------------*/
-    private static class Ballot implements Comparable<Ballot> {
+    public static class Ballot implements Comparable<Ballot> {
         private Integer seqNum;
         private Address address;
 
@@ -158,13 +224,18 @@ public class PaxosServer extends Node {
         }
 
         public int compareTo(Ballot other) {
-            if (Objects.equals(address, other.address)) {
+            if (!Objects.equals(seqNum, other.seqNum)) {
                 return seqNum.compareTo(other.seqNum);
-            } else if (Objects.equals(seqNum, other.seqNum)){
-                return address.compareTo(other.address);
             } else {
-                return convertToDouble(seqNum, address).compareTo(convertToDouble(other.seqNum, other.address));
+                return address.compareTo(other.address);
             }
+//            if (Objects.equals(address, other.address)) {
+//                return seqNum.compareTo(other.seqNum);
+//            } else if (Objects.equals(seqNum, other.seqNum)){
+//                return address.compareTo(other.address);
+//            } else {
+//                return convertToDouble(seqNum, address).compareTo(convertToDouble(other.seqNum, other.address));
+//            }
         }
 
         private Double convertToDouble(Integer seqNum, Address address) {
@@ -180,6 +251,16 @@ public class PaxosServer extends Node {
         public LogEntry(Ballot ballot, PaxosLogSlotStatus status, Command command) {
             this.ballot = ballot;
             this.paxosLogSlotStatus = status;
+            this.command = command;
+        }
+    }
+
+    private static class AcceptedEntry {
+        private Ballot ballot;
+        private Command command;
+
+        public AcceptedEntry(Ballot ballot, Command command) {
+            this.ballot = ballot;
             this.command = command;
         }
     }
